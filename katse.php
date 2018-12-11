21a8de2aa39fad2aff47f92771d67e3bc3be7996
karlsainas1 <karl.sainas@khk.ee>
2018-12-11 08:46:08 +0200

    uuendatud readme

---
+++ katse.php
@@ -0,0 +1,21 @@
+<?php
+
+echo '
+    <!doctype html>
+    <html>
+        <head>
+            <title></title>
+        </head>
+        <body>
+            <h1>PHP abil loodud HTML</h1>
+        </body>
+    </html>
+';
+?>
+
+/**
+ * Created by PhpStorm.
+ * User: karl.sainas
+ * Date: 06.12.2018
+ * Time: 11:08
+ */
